84e290c2696c2f3dadb63148b215f3087e4b65f3
Phil Mickelson <phil@cbasoftware.com>
2020-07-04 15:24:07 -0400

    update router info

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './homepage.styles.scss'
 import Directory from '../../components/directory/directory.component'
 
 const HomePage = () => (
   <div className="homepage">
+    <Link to="/hats">Hats</Link>
     <Directory className="directory-menu" />
   </div>
 )
